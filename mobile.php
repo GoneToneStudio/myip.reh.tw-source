077616eb2470329e1e76126374be11c861f8d619
REH.TW《旋風之音》 <team@reh.tw>
2015-06-22 08:33:42 +0800

    手機板網頁

---
+++ mobile.php
@@ -0,0 +1,132 @@
+<html>
+<head>
+<title>查詢IP - REH.TW</title>
+<meta name="keywords" content="REH.TW,IP,whatismyip,查IP,我的ip,ip查詢" />
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<link rel="stylesheet" href="http://yui.yahooapis.com/pure/0.6.0/pure-min.css">
+<style type="text/css">
+<!--
+.style2 {color: #003399}
+a:link {
+	font-size: 16px;
+	text-decoration: none;
+}
+.style1 {
+	color: #808000;
+}
+-->
+</style>
+</head>
+<body id="TestPage" bgcolor="#FFAA33">
+
+<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-557c114a390b18d3" async="async"></script>
+
+<br><br>
+<table width="100%" align="center">
+<tr>
+<td width="100%"><h1 align="center" class="style2" >您的 IP 是 <font color="green"><?php
+if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+   $myip = $_SERVER['HTTP_CLIENT_IP'];
+}else if(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+   $myip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+}else{
+   $myip= $_SERVER['REMOTE_ADDR'];
+}
+echo $myip;
+?></font></h1></td>
+</tr>
+<tr>
+<td align="center">方便的IP查詢網站 - myip.reh.tw</td>
+</tr>
+<tr>
+<div style="text-align:center;">
+<a href="JavaScript:window.location.reload()">重新整理</a> 
+</div>
+</tr>
+<tr>
+<td align="center"><a href="pc.php">電腦版</a> | 手機板</td>
+</tr>
+</table>
+
+<br><br><br><br><br>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<div class="fb-page" data-href="https://www.facebook.com/reh.tw.sw" data-width="100%" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/reh.tw.sw"><a href="https://www.facebook.com/reh.tw.sw">旋風之音</a></blockquote></div></div>
+<div class="fb-page" data-href="https://www.facebook.com/reh.tw" data-width="100%" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true" data-show-posts="false"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/reh.tw"><a href="https://www.facebook.com/reh.tw">改革旋風 - REH.TW</a></blockquote></div></div>
+
+<br><br><br>
+<a href="http://reh.tw" class="classname"><font size="5">REH.TW 官網</font></a>
+<br><br><br>
+
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&version=v2.3";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<br><br><br>
+<div style="text-align:center;">
+<img  hspace="5" alt="LINE@" border="0" height="120" width="120" src="http://i.imgur.com/wVOzg70.jpg" style="font-size: 16px; text-align: left; border: 0px; vertical-align: top; float: left; color: rgb(51, 51, 51); font-family: 'Hiragino Kaku Gothic Pro', '????角? Pro W3', 'ＭＳ Ｐ????', sans-serif; line-height: 27.2000007629395px;"><figcaption  class="article-info" style="text-align: left; margin: 5px 0px 0px 5px; padding: 0px; color: rgb(102, 102, 102); font-size: 12px; font-weight: bold; float: left; line-height: 22px; font-family: 'Hiragino Kaku Gothic Pro', '????角? Pro W3', 'ＭＳ Ｐ????', sans-serif;">帳號名稱：<font color="blue">[REH]改革旋風 - REH.TW</font><br />帳號類型：<font color="#FF0000">官方帳號</font><br />LINE@ID：<span  class="article-info-company-account" style="font-style: italic; color: rgb(22, 153, 5); font-family: Helvetica, Arkal, sans-serif;">@hwq3507l</span><br /><div  class="mdCMN08Img" style="margin: 0px 10px 0px 0px; padding: 0px; font-size: 13.3333339691162px; float: left; width: 118px; height: 118px; text-align: center; color: rgb(106, 117, 141); font-family: 'Hiragino Kaku Gothic ProN', Meiryo, 'MS PGothic', sans-serif; font-weight: normal; line-height: 20px;"><a  href="https://line.me/ti/p/%40hwq3507l" style="color: rgb(106, 117, 141);"><img  src="http://biz.line.naver.jp/line_business/img/btn/addfriends_zh-Hant.png" alt="好友人數" border="0" height="36" style="margin: 0px; padding: 0px; font-size: 13.3333339691162px; border: 0px; vertical-align: middle; max-width: 100%; border-radius: 3px;">
+</div>
+
+<style type="text/css">
+.classname {
+	-moz-box-shadow:inset 0px 1px 0px 0px #bbdaf7;
+	-webkit-box-shadow:inset 0px 1px 0px 0px #bbdaf7;
+	box-shadow:inset 0px 1px 0px 0px #bbdaf7;
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #79bbff), color-stop(1, #378de5) );
+	background:-moz-linear-gradient( center top, #79bbff 5%, #378de5 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#79bbff', endColorstr='#378de5');
+	background-color:#79bbff;
+	-webkit-border-top-left-radius:10px;
+	-moz-border-radius-topleft:10px;
+	border-top-left-radius:10px;
+	-webkit-border-top-right-radius:10px;
+	-moz-border-radius-topright:10px;
+	border-top-right-radius:10px;
+	-webkit-border-bottom-right-radius:10px;
+	-moz-border-radius-bottomright:10px;
+	border-bottom-right-radius:10px;
+	-webkit-border-bottom-left-radius:10px;
+	-moz-border-radius-bottomleft:10px;
+	border-bottom-left-radius:10px;
+	text-indent:0;
+	border:1px solid #84bbf3;
+	display:inline-block;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:40px;
+	font-weight:bold;
+	font-style:normal;
+	height:50px;
+	line-height:50px;
+	width:100%;
+	text-decoration:none;
+	text-align:center;
+	text-shadow:1px 1px 0px #528ecc;
+}
+.classname:hover {
+	background:-webkit-gradient( linear, left top, left bottom, color-stop(0.05, #378de5), color-stop(1, #79bbff) );
+	background:-moz-linear-gradient( center top, #378de5 5%, #79bbff 100% );
+	filter:progid:DXImageTransform.Microsoft.gradient(startColorstr='#378de5', endColorstr='#79bbff');
+	background-color:#378de5;
+}.classname:active {
+	position:relative;
+	top:1px;
+}</style>
+
+</body>
+</html>
